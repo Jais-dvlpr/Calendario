d989e0ce2209677c7de05912e5f31ebc0aed4df1
jade.py <Gtmv2002@gmail.com>
2025-09-09 12:05:19 -0700

    Agregar archivos de proyecto.

---
+++ Calendario/Program.cs
@@ -0,0 +1,30 @@
+using Calendario.Components;
+using System.Globalization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("es-MX");
+CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("es-MX");
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
